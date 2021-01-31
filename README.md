eb5698211a696027b570221b545c066463723f64
juliafum <julia.fumbarev@gmail.com>
2021-01-31 20:03:23 +0100

    fixes

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
-<h2>Data modeling with Postgres</h2>
-<h3>Summary</h3>
+##Data modeling with Postgres
+###Summary
 Using the song and log datasets, you'll need to create a star schema optimized for queries on song play analysis. This includes the following tables.
 
-* Fact Table:songplays 
-* Dimension Tables: users, songs, artists, time
+* Fact Table:
+ - songplays: songplay_id, start_time, user_id, level, song_id, artist_id, session_id, location, user_agent
+* Dimension Tables: 
+ - users: user_id, first_name, last_name, gender, level
+ - songs: song_id, title, artist_id, year, duration
+ - artists: artist_id, name, location, latitude, longitude
+ - time: start_time, hour, day, week, month, year, weekday
 
 Star schemas are denormalized, meaning the typical rules of normalization applied to transactional relational databases are relaxed during star-schema design and implementation. The benefits of star-schema denormalization are:
 
@@ -11,6 +16,14 @@ Star schemas are denormalized, meaning the typical rules of normalization applie
 * Query performance
 * Fast aggregations
 
+The project includes six files:
 
-<h3>Build ETL Pipeline</h3>
+* test.ipynb displays the first few rows of each table to let you check your database.
+* create_tables.py drops and creates your tables. You run this file to reset your tables before each time you run your ETL scripts.
+* etl.ipynb reads and processes a single file from song_data and log_data and loads the data into your tables. This notebook contains detailed instructions on the ETL process for each of the tables.
+* etl.py reads and processes files from song_data and log_data and loads them into your tables. You can fill this out based on your work in the ETL notebook.
+* sql_queries.py contains all your sql queries, and is imported into the last three files above.
+README.md provides discussion on your project.
+
+###Build ETL Pipeline
 Run create_tables.py to create/reset your tables. Run etl.py to process the entire datasets  Run test. Finally run test.ipynb to confirm your records were successfully inserted into each table.
--- sql_queries.py
+++ sql_queries.py
@@ -8,16 +8,16 @@ time_table_drop = "DROP table IF EXISTS time"
 
 # CREATE TABLES
 
-songplay_table_create = ("""CREATE TABLE IF NOT EXISTS songplays (songplay_id SERIAL PRIMARY KEY, start_time bigint, user_id varchar, level varchar, song_id varchar, artist_id varchar, session_id int, location varchar, user_agent varchar);
+songplay_table_create = ("""CREATE TABLE IF NOT EXISTS songplays (songplay_id SERIAL PRIMARY KEY, start_time timestamp not null, user_id int not null, level varchar, song_id varchar, artist_id varchar, session_id int, location varchar, user_agent varchar);
 """)
 
 user_table_create = ("""CREATE TABLE IF NOT EXISTS users (user_id int PRIMARY KEY, first_name varchar, last_name varchar, gender varchar, level varchar);
 """)
 
-song_table_create = ("""CREATE TABLE IF NOT EXISTS songs (song_id varchar PRIMARY KEY, title varchar, artist_id varchar, year int, duration real);
+song_table_create = ("""CREATE TABLE IF NOT EXISTS songs (song_id varchar PRIMARY KEY, title varchar, artist_id varchar not null, year int, duration numeric);
 """)
 
-artist_table_create = ("""CREATE TABLE IF NOT EXISTS artists (artist_id varchar PRIMARY KEY, name varchar, location varchar, latitude real, longitude real);
+artist_table_create = ("""CREATE TABLE IF NOT EXISTS artists (artist_id varchar PRIMARY KEY, name varchar not null, location varchar, latitude real, longitude real);
 """)
 
 time_table_create = ("""CREATE TABLE IF NOT EXISTS time (start_time timestamp PRIMARY KEY, hour int, day int, week int, month int, year int, weekday int);
@@ -48,5 +48,7 @@ song_select = ("""SELECT song_id, songs.artist_id FROM songs JOIN artists ON son
 
 # QUERY LISTS
 
-create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
-drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
+create_table_queries = [songplay_table_create, user_table_create,
+                        song_table_create, artist_table_create, time_table_create]
+drop_table_queries = [songplay_table_drop, user_table_drop,
+                      song_table_drop, artist_table_drop, time_table_drop]
